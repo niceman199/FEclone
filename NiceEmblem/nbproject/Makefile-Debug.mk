c7ecfae08ff91477779503117b01cf552fdc9938
niceman199 <niceman199@gmail.com>
2017-02-26 03:45:33 -0500

    Added Inventory and Item

--- NiceEmblem/nbproject/Makefile-Debug.mk
+++ NiceEmblem/nbproject/Makefile-Debug.mk
@@ -44,6 +44,8 @@ OBJECTFILES= \
 	${OBJECTDIR}/Instantiable/Instantiable.o \
 	${OBJECTDIR}/Instantiable/Map/Map.o \
 	${OBJECTDIR}/Instantiable/Map/Tile.o \
+	${OBJECTDIR}/Inventory.o \
+	${OBJECTDIR}/Item.o \
 	${OBJECTDIR}/Main/Main.o \
 	${OBJECTDIR}/Type/StatSheet.o
 
@@ -72,57 +74,67 @@ ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/niceemblem: ${OBJECTFILES}
 	${MKDIR} -p ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}
 	${LINK.cc} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/niceemblem ${OBJECTFILES} ${LDLIBSOPTIONS}
 
-${OBJECTDIR}/Instantiable/Entity/Entity.o: Instantiable/Entity/Entity.cpp 
+${OBJECTDIR}/Instantiable/Entity/Entity.o: Instantiable/Entity/Entity.cpp
 	${MKDIR} -p ${OBJECTDIR}/Instantiable/Entity
 	${RM} "$@.d"
 	$(COMPILE.cc) -g -std=c++11 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/Instantiable/Entity/Entity.o Instantiable/Entity/Entity.cpp
 
-${OBJECTDIR}/Instantiable/Entity/Structure/Structure.o: Instantiable/Entity/Structure/Structure.cpp 
+${OBJECTDIR}/Instantiable/Entity/Structure/Structure.o: Instantiable/Entity/Structure/Structure.cpp
 	${MKDIR} -p ${OBJECTDIR}/Instantiable/Entity/Structure
 	${RM} "$@.d"
 	$(COMPILE.cc) -g -std=c++11 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/Instantiable/Entity/Structure/Structure.o Instantiable/Entity/Structure/Structure.cpp
 
-${OBJECTDIR}/Instantiable/Entity/Unit/Character/Character.o: Instantiable/Entity/Unit/Character/Character.cpp 
+${OBJECTDIR}/Instantiable/Entity/Unit/Character/Character.o: Instantiable/Entity/Unit/Character/Character.cpp
 	${MKDIR} -p ${OBJECTDIR}/Instantiable/Entity/Unit/Character
 	${RM} "$@.d"
 	$(COMPILE.cc) -g -std=c++11 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/Instantiable/Entity/Unit/Character/Character.o Instantiable/Entity/Unit/Character/Character.cpp
 
-${OBJECTDIR}/Instantiable/Entity/Unit/Character/PlayableCharacter/PlayableCharacter.o: Instantiable/Entity/Unit/Character/PlayableCharacter/PlayableCharacter.cpp 
+${OBJECTDIR}/Instantiable/Entity/Unit/Character/PlayableCharacter/PlayableCharacter.o: Instantiable/Entity/Unit/Character/PlayableCharacter/PlayableCharacter.cpp
 	${MKDIR} -p ${OBJECTDIR}/Instantiable/Entity/Unit/Character/PlayableCharacter
 	${RM} "$@.d"
 	$(COMPILE.cc) -g -std=c++11 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/Instantiable/Entity/Unit/Character/PlayableCharacter/PlayableCharacter.o Instantiable/Entity/Unit/Character/PlayableCharacter/PlayableCharacter.cpp
 
-${OBJECTDIR}/Instantiable/Entity/Unit/Monster/Monster.o: Instantiable/Entity/Unit/Monster/Monster.cpp 
+${OBJECTDIR}/Instantiable/Entity/Unit/Monster/Monster.o: Instantiable/Entity/Unit/Monster/Monster.cpp
 	${MKDIR} -p ${OBJECTDIR}/Instantiable/Entity/Unit/Monster
 	${RM} "$@.d"
 	$(COMPILE.cc) -g -std=c++11 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/Instantiable/Entity/Unit/Monster/Monster.o Instantiable/Entity/Unit/Monster/Monster.cpp
 
-${OBJECTDIR}/Instantiable/Entity/Unit/Unit.o: Instantiable/Entity/Unit/Unit.cpp 
+${OBJECTDIR}/Instantiable/Entity/Unit/Unit.o: Instantiable/Entity/Unit/Unit.cpp
 	${MKDIR} -p ${OBJECTDIR}/Instantiable/Entity/Unit
 	${RM} "$@.d"
 	$(COMPILE.cc) -g -std=c++11 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/Instantiable/Entity/Unit/Unit.o Instantiable/Entity/Unit/Unit.cpp
 
-${OBJECTDIR}/Instantiable/Instantiable.o: Instantiable/Instantiable.cpp 
+${OBJECTDIR}/Instantiable/Instantiable.o: Instantiable/Instantiable.cpp
 	${MKDIR} -p ${OBJECTDIR}/Instantiable
 	${RM} "$@.d"
 	$(COMPILE.cc) -g -std=c++11 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/Instantiable/Instantiable.o Instantiable/Instantiable.cpp
 
-${OBJECTDIR}/Instantiable/Map/Map.o: Instantiable/Map/Map.cpp 
+${OBJECTDIR}/Instantiable/Map/Map.o: Instantiable/Map/Map.cpp
 	${MKDIR} -p ${OBJECTDIR}/Instantiable/Map
 	${RM} "$@.d"
 	$(COMPILE.cc) -g -std=c++11 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/Instantiable/Map/Map.o Instantiable/Map/Map.cpp
 
-${OBJECTDIR}/Instantiable/Map/Tile.o: Instantiable/Map/Tile.cpp 
+${OBJECTDIR}/Instantiable/Map/Tile.o: Instantiable/Map/Tile.cpp
 	${MKDIR} -p ${OBJECTDIR}/Instantiable/Map
 	${RM} "$@.d"
 	$(COMPILE.cc) -g -std=c++11 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/Instantiable/Map/Tile.o Instantiable/Map/Tile.cpp
 
-${OBJECTDIR}/Main/Main.o: Main/Main.cpp 
+${OBJECTDIR}/Inventory.o: Inventory.cpp
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -std=c++11 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/Inventory.o Inventory.cpp
+
+${OBJECTDIR}/Item.o: Item.cpp
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -std=c++11 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/Item.o Item.cpp
+
+${OBJECTDIR}/Main/Main.o: Main/Main.cpp
 	${MKDIR} -p ${OBJECTDIR}/Main
 	${RM} "$@.d"
 	$(COMPILE.cc) -g -std=c++11 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/Main/Main.o Main/Main.cpp
 
-${OBJECTDIR}/Type/StatSheet.o: Type/StatSheet.cpp 
+${OBJECTDIR}/Type/StatSheet.o: Type/StatSheet.cpp
 	${MKDIR} -p ${OBJECTDIR}/Type
 	${RM} "$@.d"
 	$(COMPILE.cc) -g -std=c++11 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/Type/StatSheet.o Type/StatSheet.cpp
@@ -133,7 +145,6 @@ ${OBJECTDIR}/Type/StatSheet.o: Type/StatSheet.cpp
 # Clean Targets
 .clean-conf: ${CLEAN_SUBPROJECTS}
 	${RM} -r ${CND_BUILDDIR}/${CND_CONF}
-	${RM} ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/niceemblem
 
 # Subprojects
 .clean-subprojects:
